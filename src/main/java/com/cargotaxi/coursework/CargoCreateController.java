1f5b902bd69e9ec29b4ff701353f1e6bb4969a51
satosatoo <nick.dev202@gmail.com>
2023-10-14 16:30:35 +0200

    Cargo

--- src/main/java/com/cargotaxi/coursework/CargoCreateController.java
+++ src/main/java/com/cargotaxi/coursework/CargoCreateController.java
@@ -1,4 +1,47 @@
 package com.cargotaxi.coursework;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
 public class CargoCreateController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    private TextField cargoName;
+
+    @FXML
+    private TextField dropOff;
+
+    @FXML
+    private TextField pickUp;
+
+    @FXML
+    private TextField weight;
+
+    @FXML
+    void saveToList(ActionEvent event) {
+                                                // realization
+    }
+
+    @FXML
+    void switchToCargo(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("OrderTakerController.fxml")));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
