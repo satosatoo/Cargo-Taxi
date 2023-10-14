e35505f21b139c94c066384151b6b7152cde71fe
satosatoo <nick.dev202@gmail.com>
2023-10-14 16:06:55 +0200

    added GUI only for Order Taker

--- src/main/java/com/cargotaxi/coursework/OrderTakerChangeController.java
+++ src/main/java/com/cargotaxi/coursework/OrderTakerChangeController.java
@@ -1,4 +1,49 @@
 package com.cargotaxi.coursework;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
 public class OrderTakerChangeController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    private TextField fullName;       // show name but cannot edit
+
+    @FXML
+    private TextField officeAddress;
+
+    @FXML
+    private TextField phoneNumber;
+
+    @FXML
+    void deleteOrderTaker(ActionEvent event) {
+                                                    // realization
+    }
+
+    @FXML
+    void saveChanges(ActionEvent event) {
+                                                    // realizations
+    }
+
+    @FXML
+    void switchToOrderTaker(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("OrderTakerController.fxml")));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
--- src/main/java/com/cargotaxi/coursework/OrderTakerCreateController.java
+++ src/main/java/com/cargotaxi/coursework/OrderTakerCreateController.java
@@ -1,4 +1,44 @@
 package com.cargotaxi.coursework;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
 public class OrderTakerCreateController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    private TextField fullName;
+
+    @FXML
+    private TextField officeAddress;
+
+    @FXML
+    private TextField phoneNumber;
+
+    @FXML
+    void saveToList(ActionEvent event) {
+                                            // needs realization
+    }
+
+    @FXML
+    void switchToOrderTaker(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("OrderTakerController.fxml")));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
