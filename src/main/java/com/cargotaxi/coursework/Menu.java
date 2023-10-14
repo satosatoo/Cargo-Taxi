cb07759ba281a3481e3648e68eb02075a14459e4
satosatoo <nick.dev202@gmail.com>
2023-10-14 13:56:05 +0200

    Menu

---
+++ src/main/java/com/cargotaxi/coursework/Menu.java
@@ -0,0 +1,28 @@
+package com.cargotaxi.coursework;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+import java.util.Objects;
+
+public class Menu extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("MenuController.fxml")));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
--- src/main/java/com/cargotaxi/coursework/MenuController.java
+++ src/main/java/com/cargotaxi/coursework/MenuController.java
@@ -1,5 +1,78 @@
 package com.cargotaxi.coursework;
 
-public class MenuController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MenuController implements Initializable {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    private Text date;
+
+    @FXML
+    private Text time;
+
+    @FXML
+    public void switchToCargo(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("CargoController.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void switchToContract(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("ContractController.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void switchToDriver(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("DriverController.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void switchToOrderTaker(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("OrderTakerController.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void switchToRequests(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("RequestsController.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
